df6fa6b3b5c7a2afa13b98b425d299fa95275f98
Alexander Schad <ags@vanguardistas.net>
2024-08-22 17:44:12 +0200

    SEO Fixes

--- js/portfolio.js
+++ js/portfolio.js
@@ -5,7 +5,7 @@
   let lastPos;
   let positionFromTop;
   const sections = [];
-  
+
   //  let offset = navBar.offsetTop;
   // const navrect = navBar.getBoundingClientRect();
 
@@ -18,45 +18,49 @@
 
       if (positionFromTop - windowHeight <= 0) {
         element.classList.add(element.dataset.cssclass);
-        element.classList.remove('hidden');
+        element.classList.remove("hidden");
       }
     }
-  }
-
-Math.inOutQuintic = function(t, b, c, d) {
-  var ts = (t/=d)*t,
-  tc = ts*t;
-  return b+c*(6*tc*ts + -15*ts*ts + 10*tc);
-};
-
-// requestAnimationFrame for Smart Animating http://goo.gl/sx5sts
-var requestAnimFrame = (function(){
-  return  window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function( callback ){ window.setTimeout(callback, 1000 / 60); };
-})();
-
-function scrollTo(element, to, duration) {
-  const start = element.scrollTop;
-  const change = to - start;
-  const increment = 20;
-  let currentTime = 0;
-  duration = (typeof(duration) === 'undefined') ? 500 : duration;
-  const animateScroll = function() {
-    // increment the time
-    currentTime += increment;
-    // find the value with the quadratic in-out easing function
-    const val = Math.inOutQuintic(currentTime, start, change, duration);
-    element.scrollTop = val;
-
-    // do the animation unless its over
-    if (currentTime < duration) {
-      requestAnimFrame(animateScroll);
-    }
   };
-  animateScroll();
-}
 
+  Math.inOutQuintic = function (t, b, c, d) {
+    var ts = (t /= d) * t,
+      tc = ts * t;
+    return b + c * (6 * tc * ts + -15 * ts * ts + 10 * tc);
+  };
 
+  // requestAnimationFrame for Smart Animating http://goo.gl/sx5sts
+  var requestAnimFrame = (function () {
+    return (
+      window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      function (callback) {
+        window.setTimeout(callback, 1000 / 60);
+      }
+    );
+  })();
 
+  function scrollTo(element, to, duration) {
+    const start = element.scrollTop;
+    const change = to - start;
+    const increment = 20;
+    let currentTime = 0;
+    duration = typeof duration === "undefined" ? 500 : duration;
+    const animateScroll = function () {
+      // increment the time
+      currentTime += increment;
+      // find the value with the quadratic in-out easing function
+      const val = Math.inOutQuintic(currentTime, start, change, duration);
+      element.scrollTop = val;
+
+      // do the animation unless its over
+      if (currentTime < duration) {
+        requestAnimFrame(animateScroll);
+      }
+    };
+    animateScroll();
+  }
 
   const checkNav = () => {
     // check if the navigation has reached the top then add the fixed class
@@ -64,74 +68,77 @@ function scrollTo(element, to, duration) {
     const pos = document.documentElement.scrollTop;
 
     if (pos >= positionFromTop + height && lastPos < pos) {
-      navBar.classList.add('fixed');
+      navBar.classList.add("fixed");
     }
     if (pos < positionFromTop && lastPos > pos) {
-      navBar.classList.remove('fixed');
+      navBar.classList.remove("fixed");
     }
     lastPos = pos;
-  }
+  };
 
   const checkActiveSection = () => {
     const height = navBar.getBoundingClientRect().height;
     let activeSection;
-    sections.forEach(s => {
+    sections.forEach((s) => {
       if (s.getBoundingClientRect().y - height <= 0) {
         activeSection = s;
       }
     });
-    const activeNav = document.querySelector(`.nav-link[data-target='${activeSection.id}']`);
+    const activeNav = document.querySelector(
+      `.nav-link[data-target='${activeSection.id}']`
+    );
 
-    if (!activeNav.classList.contains('active')) {
-      const oldActive = document.querySelector('.nav-link.active');
-      oldActive && oldActive.classList.remove('active');
-      activeNav.classList.add('active');
+    if (!activeNav.classList.contains("active")) {
+      const oldActive = document.querySelector(".nav-link.active");
+      oldActive && oldActive.classList.remove("active");
+      activeNav.classList.add("active");
     }
-  }
+  };
 
   const onScroll = () => {
     // do this when the window scrolls
     checkVisible();
     checkNav();
     checkActiveSection();
-  }
+  };
 
   const clickNav = (e) => {
     const targetId = e.target.dataset.target;
     const targetEl = document.getElementById(targetId);
     scrollTo(document.documentElement, targetEl.offsetTop, 300);
-    navBar.querySelector('.nav-wrapper').classList.remove('visible');
-  }
+    navBar.querySelector(".nav-wrapper").classList.remove("visible");
+  };
 
   const init = () => {
-    const downButton = document.getElementById('down-button');
-    navBar = document.querySelector('nav');
+    const downButton = document.getElementById("down-button");
+    navBar = document.querySelector("nav");
     positionFromTop = navBar.offsetTop;
-    elements = document.querySelectorAll('.hidden');
+    elements = document.querySelectorAll(".hidden");
     windowHeight = window.innerHeight;
-    about = document.getElementById('about');
-    window.addEventListener('scroll', onScroll);
+    about = document.getElementById("about");
+    window.addEventListener("scroll", onScroll);
     downButton.onclick = () => {
       scrollTo(document.documentElement, about.offsetTop, 300);
     };
-    document.querySelectorAll('.nav-link').forEach(n => {
-        n.onclick = clickNav;
-      }
-    );
-    document.querySelectorAll('section').forEach(s => {sections.push(s)});
+    document.querySelectorAll(".nav-link").forEach((n) => {
+      n.onclick = clickNav;
+    });
+    document.querySelectorAll("section").forEach((s) => {
+      sections.push(s);
+    });
 
-    const hamburger = navBar.querySelector('a.icon');
+    const hamburger = navBar.querySelector("div.icon");
     hamburger.onclick = () => {
-      navBar.querySelector('.nav-wrapper').classList.toggle('visible');
+      navBar.querySelector(".nav-wrapper").classList.toggle("visible");
     };
     checkVisible();
   };
 
   const winLoad = (callback) => {
-    if (document.readyState === 'complete') {
+    if (document.readyState === "complete") {
       callback();
     } else {
-      window.addEventListener('load', callback);
+      window.addEventListener("load", callback);
     }
   };
 
